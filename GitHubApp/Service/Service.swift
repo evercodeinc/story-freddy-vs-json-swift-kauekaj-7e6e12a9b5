bc1ac26947ad8480a50e6245fd0270192ee1cd5a
Kaue Jacyntho <kauekaj@gmail.com>
2023-02-14 15:38:54 -0300

    Implemented fetchList method

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -7,9 +7,21 @@ struct Service {
     init(network: NetworkProtocol = Network()) {
         self.network = network
     }
-
+    
     func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
         
-        // TODO
+        let url: URL = URL(fileURLWithPath: "https://api.github.com/users/\(user)/repos")
+        
+        network.performGet(url: url) { data in
+            
+            guard let data = data else {
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let repositoryList = try? decoder.decode([Repository].self, from: data)
+            
+            completion(repositoryList)
+        }
     }
 }
